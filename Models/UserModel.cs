13ac10ed9c1b4f74ba32af37be8bd2771c2c7971
56Aang <a89588@alunos.uminho.pt>
2021-06-04 18:39:26 +0100

    MVC

---
+++ Models/UserModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace WTest.Models
+{
+    public class UserModel
+    {
+
+        public string Name { get; set; }
+
+        public string Mail { get; set; }
+
+        public string Password { get; set; }
+
+        public string DateBirth { get; set; }
+    }
+
+}
