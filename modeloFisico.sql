1a84c2ea10bc4064db2d5ad214a5c5ee756bb26d
56Aang <a89588@alunos.uminho.pt>
2021-06-04 22:28:00 +0100

    modelo fisico

--- modeloFisico.sql
+++ modeloFisico.sql
@@ -1,153 +1,141 @@
--- MySQL Workbench Forward Engineering
+USE master
+GO
+IF NOT EXISTS (
+ SELECT name
+ FROM sys.databases
+ WHERE name = N'WWB'
+)
+ CREATE DATABASE [WWB];
+GO
+-- -----------------------------------------------------
+-- Table "WWB"."Utilizador"
+-- -----------------------------------------------------
 
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+-- IF OBJECT_ID('dbo.Customers', 'U') IS NOT NULL
+-- DROP TABLE dbo.Customers;
+-- GO
 
--- -----------------------------------------------------
--- Schema WWB
--- -----------------------------------------------------
-
--- -----------------------------------------------------
--- Schema WWB
--- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `WWB` DEFAULT CHARACTER SET utf8 ;
-USE `WWB` ;
-
--- -----------------------------------------------------
--- Table `WWB`.`Utilizador`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `WWB`.`Utilizador` (
-  `Id` INT NOT NULL AUTO_INCREMENT,
-  `Username` VARCHAR(30) NOT NULL,
-  `Password` VARCHAR(30) NOT NULL,
-  `Data_de_nascimento` DATE NOT NULL,
-  `Email` VARCHAR(45) NOT NULL,
-  `Nome` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`Id`),
-  UNIQUE INDEX `Username_UNIQUE` (`Username` ASC) VISIBLE,
-  UNIQUE INDEX `Email_UNIQUE` (`Email` ASC) VISIBLE)
-ENGINE = InnoDB;
+USE WWB;
+IF OBJECT_ID('dbo.Utilizador','U') IS NULL
+CREATE TABLE dbo."Utilizador" (
+  "Id" INT PRIMARY KEY IDENTITY(1,1),
+  "Username" VARCHAR(30) NOT NULL,
+  "Password" VARCHAR(30) NOT NULL,
+  "Data_de_nascimento" DATE NOT NULL,
+  "Email" VARCHAR(45) NOT NULL,
+  "Nome" VARCHAR(45) NOT NULL 
+  )
 
 
 -- -----------------------------------------------------
--- Table `WWB`.`Liga`
+-- Table "WWB"."Liga"
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `WWB`.`Liga` (
-  `Id` INT NOT NULL,
-  `Nome` VARCHAR(45) NOT NULL,
-  `Localizacao` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`Id`))
-ENGINE = InnoDB;
+IF OBJECT_ID('dbo.Liga','U') IS NULL
+CREATE TABLE dbo."Liga" (
+  "Id" INT PRIMARY KEY NOT NULL,
+  "Nome" VARCHAR(45) NOT NULL,
+  "Localizacao" VARCHAR(45) NOT NULL,
+)
 
-
 -- -----------------------------------------------------
--- Table `WWB`.`Equipa`
+-- Table "WWB"."Equipa"
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `WWB`.`Equipa` (
-  `Id` INT NOT NULL,
-  `Nome` VARCHAR(45) NOT NULL,
-  `Localizacao` VARCHAR(45) NOT NULL,
-  `Liga_Id` INT NOT NULL,
-  PRIMARY KEY (`Id`),
-  INDEX `fk_Equipa_Liga1_idx` (`Liga_Id` ASC) VISIBLE,
-  CONSTRAINT `fk_Equipa_Liga1`
-    FOREIGN KEY (`Liga_Id`)
-    REFERENCES `WWB`.`Liga` (`Id`)
+IF OBJECT_ID('dbo.Equipa','U') IS NULL
+CREATE TABLE dbo."Equipa" (
+  "Id" INT PRIMARY KEY NOT NULL,
+  "Nome" VARCHAR(45) NOT NULL,
+  "Localizacao" VARCHAR(45) NOT NULL,
+  "Liga_Id" INT NOT NULL,
+  CONSTRAINT "fk_Equipa_Liga1"
+    FOREIGN KEY ("Liga_Id")
+    REFERENCES dbo."Liga" ("Id")
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+
 
 
 -- -----------------------------------------------------
--- Table `WWB`.`Estatísticas`
+-- Table "WWB"."Estatísticas"
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `WWB`.`Estatísticas` (
-  `Pontos_a_favor` INT NOT NULL,
-  `Pontos_contra` INT NOT NULL,
-  `Vitorias` INT NOT NULL,
-  `Derrotas` INT NOT NULL,
-  `Classificacao` INT NOT NULL,
-  `Dif_pontos` INT NOT NULL,
-  `Equipa_Id` INT NOT NULL,
-  PRIMARY KEY (`Equipa_Id`),
-  INDEX `fk_Estatísticas_Equipa_idx` (`Equipa_Id` ASC) VISIBLE,
-  CONSTRAINT `fk_Estatísticas_Equipa`
-    FOREIGN KEY (`Equipa_Id`)
-    REFERENCES `WWB`.`Equipa` (`Id`)
+IF OBJECT_ID('dbo.Estatisticas','U') IS NULL
+CREATE TABLE dbo."Estatisticas" (
+  "Pontos_a_favor" INT NOT NULL,
+  "Pontos_contra" INT NOT NULL,
+  "Vitorias" INT NOT NULL,
+  "Derrotas" INT NOT NULL,
+  "Classificacao" INT NOT NULL,
+  "Dif_pontos" INT NOT NULL,
+  "Equipa_Id" INT PRIMARY KEY NOT NULL,
+  CONSTRAINT "fk_Estatísticas_Equipa"
+    FOREIGN KEY ("Equipa_Id")
+    REFERENCES dbo."Equipa" ("Id")
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+
 
 
 -- -----------------------------------------------------
--- Table `WWB`.`Jogo`
+-- Table "WWB"."Jogo"
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `WWB`.`Jogo` (
-  `Id` INT NOT NULL,
-  `Data` DATE NOT NULL,
-  `Hora` TIME NOT NULL,
-  `Resultado` VARCHAR(45) NOT NULL,
-  `Equipa_Casa` INT NOT NULL,
-  `Equipa_Visitante` INT NOT NULL,
-  PRIMARY KEY (`Id`),
-  INDEX `fk_Jogo_Equipa1_idx` (`Equipa_Casa` ASC) VISIBLE,
-  INDEX `fk_Jogo_Equipa2_idx` (`Equipa_Visitante` ASC) VISIBLE,
-  CONSTRAINT `fk_Jogo_Equipa1`
-    FOREIGN KEY (`Equipa_Casa`)
-    REFERENCES `WWB`.`Equipa` (`Id`)
+IF OBJECT_ID('dbo.Jogo','U') IS NULL
+CREATE TABLE dbo."Jogo"(
+  "Id" INT PRIMARY KEY NOT NULL,
+  "Data" DATE NOT NULL,
+  "Hora" TIME NOT NULL,
+  "Resultado" VARCHAR(45) NOT NULL,
+  "Equipa_Casa" INT NOT NULL,
+  "Equipa_Visitante" INT NOT NULL,
+  CONSTRAINT "fk_Jogo_Equipa1"
+    FOREIGN KEY ("Equipa_Casa")
+    REFERENCES dbo."Equipa" ("Id")
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_Jogo_Equipa2`
-    FOREIGN KEY (`Equipa_Visitante`)
-    REFERENCES `WWB`.`Equipa` (`Id`)
+  CONSTRAINT "fk_Jogo_Equipa2"
+    FOREIGN KEY ("Equipa_Visitante")
+    REFERENCES dbo."Equipa" ("Id")
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+
 
 
 -- -----------------------------------------------------
--- Table `WWB`.`Equipas_favoritas`
+-- Table "WWB"."Equipas_favoritas"
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `WWB`.`Equipas_favoritas` (
-  `Utilizador_Id` INT NOT NULL,
-  `Equipa_Id` INT NOT NULL,
-  PRIMARY KEY (`Utilizador_Id`, `Equipa_Id`),
-  INDEX `fk_Equipas_favoritas_Equipa1_idx` (`Equipa_Id` ASC) VISIBLE,
-  CONSTRAINT `fk_Favoritos_Utilizador1`
-    FOREIGN KEY (`Utilizador_Id`)
-    REFERENCES `WWB`.`Utilizador` (`Id`)
+IF OBJECT_ID('dbo.Equipas_Favoritas','U') IS NULL
+CREATE TABLE dbo."Equipas_Favoritas"(
+  "Utilizador_Id" INT NOT NULL,
+  "Equipa_Id" INT NOT NULL,
+  PRIMARY KEY ("Utilizador_Id", "Equipa_Id"),
+  CONSTRAINT "fk_Favoritos_Utilizador1"
+    FOREIGN KEY ("Utilizador_Id")
+    REFERENCES dbo."Utilizador" ("Id")
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_Equipas_favoritas_Equipa1`
-    FOREIGN KEY (`Equipa_Id`)
-    REFERENCES `WWB`.`Equipa` (`Id`)
+  CONSTRAINT "fk_Equipas_favoritas_Equipa1"
+    FOREIGN KEY ("Equipa_Id")
+    REFERENCES dbo."Equipa" ("Id")
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+
 
 
 -- -----------------------------------------------------
--- Table `WWB`.`Ligas_favoritas`
+-- Table "WWB"."Ligas_favoritas"
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `WWB`.`Ligas_favoritas` (
-  `Utilizador_Id` INT NOT NULL,
-  `Liga_Id` INT NOT NULL,
-  PRIMARY KEY (`Utilizador_Id`, `Liga_Id`),
-  INDEX `fk_Ligas_favoritas_Liga1_idx` (`Liga_Id` ASC) VISIBLE,
-  CONSTRAINT `fk_Ligas_favoritas_Utilizador1`
-    FOREIGN KEY (`Utilizador_Id`)
-    REFERENCES `WWB`.`Utilizador` (`Id`)
+IF OBJECT_ID('dbo.Ligas_favoritas','U') IS NULL
+CREATE TABLE dbo."Ligas_favoritas" (
+  "Utilizador_Id" INT NOT NULL,
+  "Liga_Id" INT NOT NULL,
+  PRIMARY KEY ("Utilizador_Id", "Liga_Id"),
+  CONSTRAINT "fk_Ligas_favoritas_Utilizador1"
+    FOREIGN KEY ("Utilizador_Id")
+    REFERENCES dbo."Utilizador" ("Id")
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_Ligas_favoritas_Liga1`
-    FOREIGN KEY (`Liga_Id`)
-    REFERENCES `WWB`.`Liga` (`Id`)
+  CONSTRAINT "fk_Ligas_favoritas_Liga1"
+    FOREIGN KEY ("Liga_Id")
+    REFERENCES dbo."Liga" ("Id")
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
 
 
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
